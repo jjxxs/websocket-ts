86b0e66e0172ffc66e079636dd588043939d0934
Joscha Behrmann <behrmann@hm.edu>
2020-09-11 22:18:28 +0200

    Renamed file

--- test/backoff/constantbackoff.test.ts
+++ test/backoff/constantbackoff.test.ts
@@ -1,4 +1,4 @@
-import {ConstantBackoff, ExponentialBackoff} from "../../src";
+import {ConstantBackoff} from "../../src/backoff/constantbackoff";
 
 describe("Testsuite for ConstantBackoff", () => {
     const delay = 5;
@@ -14,18 +14,5 @@ describe("Testsuite for ConstantBackoff", () => {
         backoff.Reset();
         for (let i = 0; i < 10; i++)
             expect(backoff.Next()).toBe(delay);
-    });
-})
-
-describe("Testsuite for ExponentialBackoff", () => {
-    const p = 0, expMin = 0, expMax = 0;
-    let backoff: ExponentialBackoff;
-
-    beforeEach(() => {
-       backoff = new ExponentialBackoff(p, expMin, expMax);
-    });
-
-    test("ExponentialBackoff should increase exponentially", () => {
-
     });
 })
