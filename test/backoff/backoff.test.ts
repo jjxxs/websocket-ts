86ea24d9417dde198d7e8b987980e6c743a56865
Joscha Behrmann <behrmann@hm.edu>
2020-09-11 15:05:45 +0200

    Added tests for backoff

---
+++ test/backoff/backoff.test.ts
@@ -0,0 +1,31 @@
+import {ConstantBackoff, ExponentialBackoff} from "../../src/backoff/backoff";
+
+describe("Testsuite for ConstantBackoff", () => {
+    const delay = 5;
+    let backoff: ConstantBackoff;
+
+    beforeEach(() => {
+        backoff = new ConstantBackoff(delay);
+    });
+
+    test("ConstantBackoff should always return the same value", () => {
+        for (let i = 0; i < 10; i++)
+            expect(backoff.Next()).toBe(delay);
+        backoff.Reset();
+        for (let i = 0; i < 10; i++)
+            expect(backoff.Next()).toBe(delay);
+    });
+})
+
+describe("Testsuite for ExponentialBackoff", () => {
+    const p = 0, expMin = 0, expMax = 0;
+    let backoff: ExponentialBackoff;
+
+    beforeEach(() => {
+       backoff = new ExponentialBackoff(p, expMin, expMax);
+    });
+
+    test("ExponentialBackoff should increase exponentially", () => {
+
+    });
+})
