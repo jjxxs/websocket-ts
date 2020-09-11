1e777b7e520c7914954359b02fea5306fa40a1a6
Joscha Behrmann <behrmann@hm.edu>
2020-09-11 22:17:28 +0200

    Remove expired items on Len()-calls

--- src/buffer/timebuffer.ts
+++ src/buffer/timebuffer.ts
@@ -20,6 +20,7 @@ export class TimeBuffer<E> implements Buffer<E> {
     }
 
     Len(): number {
+        this.forwardTail();
         let cur = this.tail;
         let i = 0;
         while (cur !== undefined) {
