8b789a892fec7c2e5cc655c1ce32b6368e983254
Joscha Behrmann <behrmann@hm.edu>
2020-09-11 22:16:54 +0200

    Added documentation

--- src/backoff/backoff.ts
+++ src/backoff/backoff.ts
@@ -1,69 +1,18 @@
+/**
+ * Backoff provides a series of numbers. This series follows
+ * a concrete pattern, e.g. be constant, linearly increasing,
+ * exponentially increasing etc.
+ */
 export interface Backoff {
+    /**
+     * Provides the callee with the next number in the
+     * series.
+     * @return the next number
+     */
     Next(): number
 
+    /**
+     *  Resets the series to its starting-value.
+     */
     Reset(): void
-}
-
-export class ConstantBackoff implements Backoff {
-    private readonly backoff: number;
-
-    constructor(backoff: number) {
-        this.backoff = backoff;
-    }
-
-    Next(): number {
-        return this.backoff;
-    }
-
-    Reset = () => {
-    }
-}
-
-export class ExponentialBackoff implements Backoff {
-    private readonly p: number;
-    private readonly expMin: number;
-    private readonly expMax: number;
-    private expCur: number;
-
-    constructor(p: number, expMin: number, expMax: number) {
-        this.p = p;
-        this.expMin = expMin;
-        this.expMax = expMax;
-        this.expCur = expMin;
-    }
-
-    Next(): number {
-        const next = this.p * Math.pow(2, this.expCur);
-        if (this.expMax > this.expCur)
-            this.expCur++;
-        return next;
-    }
-
-    Reset() {
-        this.expCur = this.expMin;
-    }
-}
-
-export class LinearBackoff implements Backoff {
-    private readonly p: number;
-    private readonly kMax?: number;
-    private k: number = 1;
-
-    constructor(p: number, kMax?: number) {
-        this.p = p;
-        this.kMax = kMax;
-    }
-
-    Next() {
-        const next = this.k * this.p;
-        if (this.kMax === undefined)
-            this.k++;
-        else if (this.kMax > this.k)
-            this.k++;
-        return next;
-    }
-
-    Reset() {
-        this.k = 0;
-    }
 }
