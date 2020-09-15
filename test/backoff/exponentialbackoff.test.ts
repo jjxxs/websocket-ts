a4ebb941dd6534457a6d9b70b4da82f806d6533f
J. Behrmann <jjxxs@users.noreply.github.com>
2020-09-15 15:21:35 +0200

    Test (#6)
    * Added tests for Builder
    
    * Added test for ExponentialBackoff
    
    * Updated README.md
    
    * Added default-values for constructor-parameters
    
    * Bumped version to rc5

--- test/backoff/exponentialbackoff.test.ts
+++ test/backoff/exponentialbackoff.test.ts
@@ -1,14 +1,26 @@
 import {ExponentialBackoff} from "../../src/backoff/exponentialbackoff";
 
 describe("Testsuite for ExponentialBackoff", () => {
-    const p = 0, expMin = 0, expMax = 0;
+    const k = 0, expMin = 0, expMax = 8;
     let backoff: ExponentialBackoff;
 
     beforeEach(() => {
-       backoff = new ExponentialBackoff(p, expMin, expMax);
+       backoff = new ExponentialBackoff(k, expMin, expMax);
     });
 
     test("ExponentialBackoff should increase exponentially", () => {
-        expect(true).toBe(true);
+        let expCur = expMin;
+        const calc = (): number => {
+            const next = k * Math.pow(2, expCur);
+            if (expMax > expCur)
+                expCur++;
+            return next;
+        }
+        for (let i = 0; i < expMax + 5; i++)
+            expect(backoff.Next()).toBe(calc());
+        backoff.Reset()
+        expCur = expMin;
+        for (let i = 0; i < expMax + 5; i++)
+            expect(backoff.Next()).toBe(calc());
     });
 });
