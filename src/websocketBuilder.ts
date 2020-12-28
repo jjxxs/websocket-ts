9b98fb7caca46e9f08e4d3da504c649f08cdaf31
Joscha Behrmann <behrmann@hm.edu>
2020-12-28 20:00:49 +0100

    Changed callbacks from optional to nullable

--- src/websocketBuilder.ts
+++ src/websocketBuilder.ts
@@ -2,82 +2,85 @@ import {Backoff} from "./backoff/backoff";
 import {Buffer} from "./buffer/buffer";
 import {RetryEventDetails, Websocket, WebsocketEvents} from "./websocket";
 
-export class WsBuilder {
+/**
+ * Used to build Websocket-instances.
+ */
+export class WebsocketBuilder {
     private readonly url: string;
-    private protocols?: string | string[]
-    private backoff?: Backoff
-    private buffer?: Buffer<string | ArrayBufferLike | Blob | ArrayBufferView>
-    private onOpenChain?: (instance: Websocket, ev: Event) => any;
-    private onCloseChain?: (instance: Websocket, ev: CloseEvent) => any;
-    private onErrorChain?: (instance: Websocket, ev: Event) => any;
-    private onMessageChain?: (instance: Websocket, ev: MessageEvent) => any;
-    private onRetryChain?: (instance: Websocket, ev: CustomEvent<RetryEventDetails>) => any;
-    private ws?: Websocket;
+    private protocols?: string | string[];
+    private backoff?: Backoff;
+    private buffer?: Buffer<string | ArrayBufferLike | Blob | ArrayBufferView>;
+    private onOpenChain: ((instance: Websocket, ev: Event) => any) | null = null;
+    private onCloseChain: ((instance: Websocket, ev: CloseEvent) => any) | null = null;
+    private onErrorChain: ((instance: Websocket, ev: Event) => any) | null = null;
+    private onMessageChain: ((instance: Websocket, ev: MessageEvent) => any) | null = null;
+    private onRetryChain: ((instance: Websocket, ev: CustomEvent<RetryEventDetails>) => any) | null = null;
+    private ws: Websocket | null = null;
 
     constructor(url: string) {
         this.url = url;
     }
 
-    public withProtocols(p: string | string[]): WsBuilder {
+    public withProtocols(p: string | string[]): WebsocketBuilder {
         this.protocols = p;
         return this;
     }
 
-    public withBackoff(backoff: Backoff): WsBuilder {
+    public withBackoff(backoff: Backoff): WebsocketBuilder {
         this.backoff = backoff;
         return this;
     }
 
-    public withBuffer(buffer: Buffer<any>): WsBuilder {
+    public withBuffer(buffer: Buffer<any>): WebsocketBuilder {
         this.buffer = buffer;
         return this;
     }
 
-    public onOpen(fn: (instance: Websocket, ev: Event) => any): WsBuilder {
+    public onOpen(fn: (instance: Websocket, ev: Event) => any): WebsocketBuilder {
         const onOpen = this.onOpenChain;
         this.onOpenChain = (instance: Websocket, ev2: Event) => {
             fn(instance, ev2);
-            if (onOpen !== undefined)
+            if (onOpen !== null)
                 onOpen(instance, ev2);
         }
         return this;
     }
 
-    public onClose(fn: (instance: Websocket, ev: CloseEvent) => any): WsBuilder {
+    public onClose(fn: (instance: Websocket, ev: CloseEvent) => any): WebsocketBuilder {
         const onClose = this.onCloseChain;
         this.onCloseChain = (instance: Websocket, ev2: CloseEvent) => {
             fn(instance, ev2);
-            if (onClose !== undefined)
+            if (onClose !== null)
                 onClose(instance, ev2);
         }
         return this;
     }
 
-    public onError(fn: (instance: Websocket, ev: Event) => any): WsBuilder {
+    public onError(fn: (instance: Websocket, ev: Event) => any): WebsocketBuilder {
         const onError = this.onErrorChain;
         this.onErrorChain = (instance: Websocket, ev2: Event) => {
             fn(instance, ev2);
-            if (onError !== undefined)
+            if (onError !== null)
                 onError(instance, ev2);
         }
         return this;
     }
 
-    public onMessage(fn: (instance: Websocket, ev: MessageEvent) => any): WsBuilder {
+    public onMessage(fn: (instance: Websocket, ev: MessageEvent) => any): WebsocketBuilder {
         const onMessage = this.onMessageChain;
         this.onMessageChain = (instance: Websocket, ev2: MessageEvent) => {
             fn(instance, ev2);
-            if (onMessage !== undefined)
+            if (onMessage !== null)
                 onMessage(instance, ev2);
         }
         return this;
     }
 
-    public onRetry(fn: (instance: Websocket, ev: CustomEvent<RetryEventDetails>) => any): WsBuilder {
+    public onRetry(fn: (instance: Websocket, ev: CustomEvent<RetryEventDetails>) => any): WebsocketBuilder {
         const onRetry = this.onRetryChain;
         this.onRetryChain = (instance: Websocket, ev2: CustomEvent<RetryEventDetails>) => {
             fn(instance, ev2);
-            if (onRetry !== undefined)
+            if (onRetry !== null)
                 onRetry(instance, ev2);
         }
         return this;
@@ -87,18 +90,18 @@ export class WsBuilder {
      * Multiple calls to build() will always return the same websocket-instance.
      */
     public build(): Websocket {
-        if (this.ws !== undefined)
+        if (this.ws !== null)
             return this.ws;
         this.ws = new Websocket(this.url, this.protocols, this.buffer, this.backoff);
-        if (this.onOpenChain !== undefined)
+        if (this.onOpenChain !== null)
             this.ws.addEventListener(WebsocketEvents.open, this.onOpenChain);
-        if (this.onCloseChain !== undefined)
+        if (this.onCloseChain !== null)
             this.ws.addEventListener(WebsocketEvents.close, this.onCloseChain);
-        if (this.onErrorChain !== undefined)
+        if (this.onErrorChain !== null)
             this.ws.addEventListener(WebsocketEvents.error, this.onErrorChain);
-        if (this.onMessageChain !== undefined)
+        if (this.onMessageChain !== null)
             this.ws.addEventListener(WebsocketEvents.message, this.onMessageChain);
-        if (this.onRetryChain !== undefined)
+        if (this.onRetryChain !== null)
             this.ws.addEventListener(WebsocketEvents.retry, this.onRetryChain);
         return this.ws;
     }
