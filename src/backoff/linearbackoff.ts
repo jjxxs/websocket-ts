61c1e2954528ab6e8498c017ab94e292b9bf837e
Joscha Behrmann <behrmann@hm.edu>
2020-12-28 20:04:22 +0100

    Fixed behaviour

--- src/backoff/linearbackoff.ts
+++ src/backoff/linearbackoff.ts
@@ -1,25 +1,37 @@
 import {Backoff} from "./backoff";
 
+/**
+ * LinearBackoff increases the backoff-time by a constant number with
+ * every step. An optional maximum can be provided as an upper bound
+ * to the returned backoff.
+ *
+ * Example: for initial=0, increment=2000, maximum=8000 the Linear-
+ * Backoff will produce the series [0, 2000, 4000, 6000, 8000].
+ */
 export class LinearBackoff implements Backoff {
-    private readonly p: number;
-    private readonly kMax?: number;
-    private k: number = 1;
+    private readonly initial: number;
+    private readonly increment: number;
+    private readonly maximum?: number;
+    private current: number;
 
-    constructor(p: number, kMax?: number) {
-        this.p = p;
-        this.kMax = kMax;
+    constructor(initial: number, increment: number, maximum?: number) {
+        this.initial = initial;
+        this.increment = increment;
+        this.maximum = maximum;
+        this.current = this.initial;
     }
 
     Next() {
-        const next = this.k * this.p;
-        if (this.kMax === undefined)
-            this.k++;
-        else if (this.kMax > this.k)
-            this.k++;
-        return next;
+        const backoff = this.current;
+        const next = this.current + this.increment;
+        if (this.maximum === undefined)
+            this.current = next;
+        else if (next <= this.maximum)
+            this.current = next;
+        return backoff;
     }
 
     Reset() {
-        this.k = 0;
+        this.current = this.initial;
     }
 }
