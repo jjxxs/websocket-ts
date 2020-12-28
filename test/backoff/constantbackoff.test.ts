812855223d563e6966e26cb30c58f6aa3f35ea7d
Joscha Behrmann <behrmann@hm.edu>
2020-12-28 20:02:52 +0100

    Cleanup

--- test/backoff/constantbackoff.test.ts
+++ test/backoff/constantbackoff.test.ts
@@ -1,32 +1,21 @@
-import {ConstantBackoff} from "../../src/backoff/constantbackoff";
-import {ExponentialBackoff} from "../../src/backoff/exponentialbackoff";
+import {ConstantBackoff} from "../../src";
 
 describe("Testsuite for ConstantBackoff", () => {
-    const delay = 5;
-    let backoff: ConstantBackoff;
-
-    beforeEach(() => {
-        backoff = new ConstantBackoff(delay);
-    });
-
-    test("ConstantBackoff should always return the same value", () => {
+    test("ConstantBackoff should always return the same value 0", () => {
+        const sut = new ConstantBackoff(0);
         for (let i = 0; i < 10; i++)
-            expect(backoff.Next()).toBe(delay);
-        backoff.Reset();
+            expect(sut.Next()).toBe(0);
+        sut.Reset();
         for (let i = 0; i < 10; i++)
-            expect(backoff.Next()).toBe(delay);
-    });
-})
-
-describe("Testsuite for ExponentialBackoff", () => {
-    const p = 0, expMin = 0, expMax = 0;
-    let backoff: ExponentialBackoff;
-
-    beforeEach(() => {
-       backoff = new ExponentialBackoff(p, expMin, expMax);
+            expect(sut.Next()).toBe(0);
     });
 
-    test("ExponentialBackoff should increase exponentially", () => {
-
+    test("ConstantBackoff should always return the same value 1000", () => {
+        const sut = new ConstantBackoff(1000);
+        for (let i = 0; i < 10; i++)
+            expect(sut.Next()).toBe(1000);
+        sut.Reset();
+        for (let i = 0; i < 10; i++)
+            expect(sut.Next()).toBe(1000);
     });
 })
