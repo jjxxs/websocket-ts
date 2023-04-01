4f393d6878e9032133cd2ffd41c4a59ea76f3ffc
Joscha Behrmann <behrmann@hm.edu>
2023-04-01 18:06:02 +0200

    Improved documentation, readability and test-coverage

--- src/backoff/exponentialbackoff.ts
+++ src/backoff/exponentialbackoff.ts
@@ -1,38 +1,69 @@
-import {Backoff} from "./backoff";
+import {Backoff} from "./backoff"
 
 /**
- * ExponentialBackoff doubles the backoff with every step until a maximum
- * is reached. This is modelled after the binary exponential-backoff algo-
- * rithm used in computer-networking.
+ * ExponentialBackoff increases the backoff-time exponentially.
+ * An optional maximum can be provided as an upper bound to the
+ * exponent and thus to the returned backoff.
  *
- * The calculation-specification is:
- *          backoff = k * 2^s with s in [1, expMax].
+ * The series can be described as ('i' is the current step/retry):
+ *    backoff = base * 2^i                | without bound
+ *    backoff = base * 2^min(i, expMax)   | with bound
  *
- * Example: for initial=100, expMax=7 the ExponentialBackoff will pro-
- * duce the backoff-series [100, 200, 400, 800, 1600, 3200, 6400].
+ * Example:
+ *
+ * 1) Without bound:
+ *   base = 1000, expMax = undefined
+ *   backoff = 1000 * 2^0 = 1000 // first retry
+ *   backoff = 1000 * 2^1 = 2000 // second retry
+ *   backoff = 1000 * 2^2 = 4000 // ...doubles with every retry
+ *   backoff = 1000 * 2^3 = 8000
+ *   backoff = 1000 * 2^4 = 16000
+ *   ... // and so on
+ *
+ * 2) With bound:
+ *  base = 1000, expMax = 3
+ *  backoff = 1000 * 2^0 = 1000 // first retry
+ *  backoff = 1000 * 2^1 = 2000 // second retry
+ *  backoff = 1000 * 2^2 = 4000 // third retry
+ *  backoff = 1000 * 2^3 = 8000 // maximum reached, don't increase further
+ *  backoff = 1000 * 2^3 = 8000
+ *  backoff = 1000 * 2^3 = 8000
+ *  ... // and so on
+ *
  */
 export class ExponentialBackoff implements Backoff {
-    private readonly initial: number;
-    private readonly expMax: number;
-    private expCurrent: number;
-    private current: number;
+    private readonly base: number
+    private readonly expMax?: number
+    private i: number
 
-    constructor(initial: number, expMax: number) {
-        this.initial = initial;
-        this.expMax = expMax;
-        this.expCurrent = 1;
-        this.current = this.initial;
+    /**
+     * Creates a new ExponentialBackoff.
+     * @param base the base of the exponentiation
+     * @param expMax the maximum exponent, no bound if undefined
+     */
+    constructor(base: number, expMax?: number) {
+        if (base < 0) {
+            throw new Error("base must be >= 0")
+        }
+        if (expMax !== undefined && expMax < 0) {
+            throw new Error("expMax must be >= 0")
+        }
+
+        this.base = base
+        this.expMax = expMax
+        this.i = 0
+    }
+
+    current(): number {
+        return this.base * Math.pow(2, this.i)
     }
 
     next(): number {
-        const backoff = this.current;
-        if (this.expMax > this.expCurrent++)
-            this.current = this.current * 2;
-        return backoff;
+        this.i = this.expMax === undefined ? this.i + 1 : Math.min(this.i + 1, this.expMax)
+        return this.current()
     }
 
     reset() {
-        this.expCurrent = 1;
-        this.current = this.initial;
+        this.i = 0
     }
 }
--- test/backoff/constantbackoff.test.ts
+++ test/backoff/constantbackoff.test.ts
@@ -1,21 +1,39 @@
 import {ConstantBackoff} from "../../src";
 
 describe("Testsuite for ConstantBackoff", () => {
-    test("ConstantBackoff should always return the same value 0", () => {
-        const sut = new ConstantBackoff(0);
-        for (let i = 0; i < 10; i++)
-            expect(sut.next()).toBe(0);
-        sut.reset();
-        for (let i = 0; i < 10; i++)
-            expect(sut.next()).toBe(0);
-    });
 
-    test("ConstantBackoff should always return the same value 1000", () => {
-        const sut = new ConstantBackoff(1000);
-        for (let i = 0; i < 10; i++)
-            expect(sut.next()).toBe(1000);
-        sut.reset();
-        for (let i = 0; i < 10; i++)
-            expect(sut.next()).toBe(1000);
-    });
+    test("Initialization should throw on negative backoff", () => {
+        expect(() => new ConstantBackoff(-1001)).toThrow()
+        expect(() => new ConstantBackoff(-42)).toThrow()
+        expect(() => new ConstantBackoff(-1)).toThrow()
+    })
+
+    test("Initialization should not throw on zero backoff", () => {
+        expect(() => new ConstantBackoff(0)).not.toThrow()
+    })
+
+    test("Initialization should not throw on positive backoff", () => {
+        expect(() => new ConstantBackoff(1)).not.toThrow()
+        expect(() => new ConstantBackoff(42)).not.toThrow()
+        expect(() => new ConstantBackoff(1001)).not.toThrow()
+    })
+
+    test("Backoff should be equal to the given backoff", () => {
+        expect(new ConstantBackoff(1).current()).toBe(1)
+        expect(new ConstantBackoff(42).current()).toBe(42)
+        expect(new ConstantBackoff(1001).current()).toBe(1001)
+    })
+
+    test("Backoff should be equal to the given backoff after next", () => {
+        const backoff = new ConstantBackoff(42)
+        backoff.next()
+        expect(backoff.current()).toBe(42)
+    })
+
+    test("Backoff should be equal to the given backoff after reset", () => {
+        const backoff = new ConstantBackoff(42)
+        backoff.next()
+        backoff.reset()
+        expect(backoff.current()).toBe(42)
+    })
 })
