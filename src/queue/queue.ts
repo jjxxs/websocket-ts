ce8ad0a64e7645f3048e4b3780442251869ee284
Joscha Behrmann <behrmann@hm.edu>
2023-04-01 20:17:44 +0200

    Restructured buffer/queue api, added implementations & tests

---
+++ src/queue/queue.ts
@@ -0,0 +1,46 @@
+/**
+ * A queue holds elements until they are read. The order in which elements are
+ * read is determined by the implementation of the queue.
+ */
+export interface Queue<E> {
+    /**
+     * Adds an element to the queue.
+     * @param element the element to add
+     */
+    add(element: E): void
+
+    /**
+     * Clears the queue, removing all elements.
+     */
+    clear(): void
+
+    /**
+     * Calls the given function for each element in the queue.
+     * @param fn the function to call
+     */
+    forEach(fn: (element: E) => any): void
+
+    /**
+     * Number of elements in the queue.
+     * @return the number of elements in the queue
+     */
+    length(): number
+
+    /**
+     * Whether the queue is empty.
+     * @return true if the queue is empty, false otherwise
+     */
+    isEmpty(): boolean
+
+    /**
+     * Returns the next element in the queue without removing it.
+     * @return the next element in the queue, or undefined if the queue is empty
+     */
+    peek(): E | undefined
+
+    /**
+     * Removes and returns the next element in the queue.
+     * @return the next element in the queue, or undefined if the queue is empty
+     */
+    read(): E | undefined
+}
