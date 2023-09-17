050988e772f19239c0b0dce14260425b9c90e1f6
jjxxs <joscha.behrmann@j-be.eu>
2023-09-17 16:52:18 +0200

    Refactor index.ts to export only specific classes and types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-export * from "./backoff/backoff";
-export * from "./backoff/constantbackoff";
-export * from "./backoff/exponentialbackoff";
-export * from "./backoff/linearbackoff";
-export * from "./queue/queue";
-export * from "./queue/array_queue";
-export * from "./queue/ring_queue";
-export * from "./websocket";
-export * from "./websocket_buffer";
-export * from "./websocket_builder";
-export * from "./websocket_event";
-export * from "./websocket_options";
-export * from "./websocket_retry_options";
+export { Backoff } from "./backoff/backoff";
+export { ConstantBackoff } from "./backoff/constantbackoff";
+export { ExponentialBackoff } from "./backoff/exponentialbackoff";
+export { LinearBackoff } from "./backoff/linearbackoff";
+export { Queue } from "./queue/queue";
+export { ArrayQueue } from "./queue/array_queue";
+export { RingQueue } from "./queue/ring_queue";
+export { Websocket } from "./websocket";
+export { WebsocketBuffer } from "./websocket_buffer";
+export { WebsocketBuilder } from "./websocket_builder";
+export {
+  WebsocketEvent,
+  RetryEventDetail,
+  ReconnectEventDetail,
+  WebsocketEventMap,
+  WebsocketEventListener,
+  WebsocketEventListenerParams,
+  WebsocketEventListenerOptions,
+  WebsocketEventListenerWithOptions,
+  WebsocketEventListeners,
+} from "./websocket_event";
+export { WebsocketOptions } from "./websocket_options";
+export { WebsocketConnectionRetryOptions } from "./websocket_retry_options";
